d35c95a23b8d85ebb11bf04f78e90715bdb9fe91
Ben Chou <ben7152000@gmail.com>
2021-07-31 12:29:44 +0800

    category route create

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 const auth = require('./auth')
 const user = require('./user')
 const category = require('./category')
+const post = require('./post')
 
 // 註冊、登入
 router.post('/api/auth/register', auth.register)
@@ -15,5 +16,12 @@ router.delete('/api/users/:id', user.deleteUser)
 // 種類操作
 router.get('/api/categories', category.getAllCategories)
 router.post('/api/categories', category.postCategory)
+
+// 貼文操作
+router.post('/api/posts', post.createPost)
+router.get('/api/posts/', post.getAllPost)
+router.get('/api/posts/:id', post.getPost)
+router.put('/api/posts/:id', post.updatePost)
+router.delete('/api/posts/:id', post.deletePost)
 
 module.exports = router
