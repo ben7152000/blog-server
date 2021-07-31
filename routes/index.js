2ebe5abf689828fea382a3d7c743375b851bcec1
Ben Chou <ben7152000@gmail.com>
2021-07-31 12:42:34 +0800

    upload image

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,21 @@
 const router = require('express').Router()
+const multer = require('multer')
 const auth = require('./auth')
 const user = require('./user')
 const category = require('./category')
 const post = require('./post')
+
+// 圖片上傳資訊
+const storage = multer.diskStorage({
+  destination: function (req, file, cd) {
+    cd(null, './uploads')
+  },
+  filename: function (req, file, cd) {
+    cd(null, file.fieldname + '_' + Date.now() + '_' + file.originalname)
+  }
+})
+
+const upload = multer({ storage: storage }).single('image')
 
 // 註冊、登入
 router.post('/api/auth/register', auth.register)
@@ -23,5 +36,8 @@ router.get('/api/posts/', post.getAllPost)
 router.get('/api/posts/:id', post.getPost)
 router.put('/api/posts/:id', post.updatePost)
 router.delete('/api/posts/:id', post.deletePost)
+
+// 圖片上傳
+router.post('/api/upload', upload, (req, res) => { res.status(200).json({ message: '圖片上傳成功' }) })
 
 module.exports = router
